eb03970d8848ae341c0ba7bb8d73b2ff023b69a9
sporubcan <sporubcan@gmail.com>
2024-04-05 21:02:44 +0200

    Created simpleTest.py

---
+++ simpleTest.py
@@ -0,0 +1,59 @@
+import MP71077x
+import time
+
+load = MP71077x.MP71077x("192.168.88.80", "192.168.88.82", verbosity=True)
+load.openSocket()
+
+load.setUpperVoltageLimit(30.00)
+load.setUpperCurrentLimit(30.00)
+
+try:
+    load.setCIcurrent(40.000, True)
+except Exception as e:
+    print (e)
+    pass
+load.setUpperCurrentLimit(40.00)
+load.setCIcurrent(40.000, True)
+
+time.sleep(1)
+
+try:
+    load.setCVvoltage(40.000, True)
+except Exception as e:
+    print (e)
+    pass
+load.setUpperVoltageLimit(150.00)
+load.setCVvoltage(150.000, True)
+
+time.sleep(1)
+
+load.setUpperPowerLimit(25.00)
+try:
+    load.setCPpower(35.000, True)
+except Exception as e:
+    print (e)
+    pass
+load.setUpperPowerLimit(300.00)
+load.setCPpower(300.000, True)
+
+time.sleep(1)
+
+load.setUpperResistanceLimit(133.00)
+try:
+    load.setCRresistance(500.000, True)
+except Exception as e:
+    print (e)
+    pass
+load.setUpperResistanceLimit(7500.00)
+load.setCRresistance(7500.000, True)
+
+load.turnInputON(True)
+time.sleep(2)
+load.turnInputOFF(True)
+
+print(load.getVoltageLimits())
+print(load.getCurrentLimits())
+print(load.getPowerLimits())
+print(load.getResistanceLimits())
+
+load.closeSocket()
